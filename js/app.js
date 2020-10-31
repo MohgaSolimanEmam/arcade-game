0bd66e97db4d09b84101211bf5db666307764796
Mohga Emam <42916236+MohgaSolimanEmam@users.noreply.github.com>
2020-10-31 06:05:14 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ var Enemy = function(x,y,speed) {
     // we've provided one for you to get started
     this.x = x;
     this.y = y;
-    this.speed = speed+200;
+    this.speed = speed*200;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -17,9 +17,9 @@ Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += (this.speed * dt);
+    this.x *= (this.speed * dt);
     if (this.x > 505) {
-        this.x += 5;
+        this.x *= 5;
         this.speed = Math.floor(Math.random() * 512);
     }
     if (player.x < this.x + 25 &&
